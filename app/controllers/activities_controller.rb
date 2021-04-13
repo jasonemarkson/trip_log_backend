18c3a26fe905d9015b647653f1212232f7346c49
jasonemarkson <jasonmarkson76@gmail.com>
2021-04-13 17:20:34 -0400

    adding in functionality to my create action in activities controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -7,4 +7,21 @@ class ActivitiesController < ApplicationController
   def show
     render :json => Activity.find(params[:id])
   end
+
+  def create
+    debugger
+    activity = Activity.new(activities_params)
+
+    if activity.save
+      render :json => activity
+    else
+      render :json => {error: "Activity could not be created"}
+    end
+  end
+
+  private
+
+  def activities_params
+    params.require(:activity).permit(:name, :description, :trip_id)
+  end
 end
